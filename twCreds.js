0a94faa9ad6c99269b52969bc096fa89fadf9bae
192-sean <192.sean.luke@gmail.com>
2014-03-17 00:09:35 -0500

    Credentials default file

---
+++ twCreds.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TWILIO_SID: 'TWILIO_SID_HERE',
+  TWILIO_AUTHTOKEN: 'TWILIO_AUTHTOKEN_HERE',
+  TWILIO_NUMBER: 'YOUR_TWILIO_NUMBER',
+  TWILIO_HOSTNAME: 'htpps://YOUR_ACCOUNT.ngrok.com',
+};
+
+if(module.exports.TWILIO_SID == 'TWILIO_SID_HERE'){
+  console.log('WRONG WRONG WRONG WRONG WRONG!!!!!!!');
+  console.log('You forget to put your twilio creds into twCreds.js. Please do that and try again.');
+}
